cb5069dbc0ef434ac647c0aa0578730b68d83cde
Julio Matias <juliocesark2@hotmail.com>
2024-01-31 23:57:33 -0300

    feat: implementando server.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = process.env.PORT || '3001';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
-});
+module.exports = app;
