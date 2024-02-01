d524f26e19894e1805c7d67a5066584350d96c1f
Julio Matias <juliocesark2@hotmail.com>
2024-02-01 01:22:02 -0300

    feat: req 01 ok

--- src/routes/talkerRoute.js
+++ src/routes/talkerRoute.js
@@ -1,18 +1,30 @@
 const { Router } = require('express');
 const fs = require('fs').promises;
 const path = require('path');
+const dotenv = require('dotenv');
+// const connection = require('../db/connection');
+
+dotenv.config();
 
 const talkerRoute = Router();
 
-const getTalkersDB = async () => {
+const getTalkersJson = async () => {
   const talkers = JSON.parse(
     await fs.readFile(path.resolve(__dirname, '../talker.json'), 'utf-8'),
   );
   return talkers;
 };
 
+// conexão com o banco de dados através do arquivo connection.js
+// const getTalkersFromDB = async () => {
+//   const [talkers] = await connection.execute('SELECT * FROM talkers');
+//   return talkers;
+// };
+
 const getTalkers = async (_req, res) => {
-  const talkers = await getTalkersDB();
+  const talkers = await getTalkersJson();
+  // const talkersDB = await getTalkersFromDB();
+  // console.log('talkersDB:', talkersDB);
 
   if (!talkers) return res.status(200).json([]);
 
